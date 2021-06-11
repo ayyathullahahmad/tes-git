f460fa88ad948977230f96b2410e0395c386e403
ayyathullah <ayyathullahahmad@gmail.com>
2021-06-11 09:02:36 +0700

    Initial Commit

---
+++ tes git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tes git
+//
+//  Created by Ayyathullah Ahmad on 11/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
